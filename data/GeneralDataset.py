82be71ee6bb7c0f377614a26dcb3cf4fdc3cc240
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-04-27 10:37:20 +0300

    Added dataset

---
+++ data/GeneralDataset.py
@@ -0,0 +1,21 @@
+from torch.utils.data import Dataset
+
+
+class NIPSDataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+
+class DatasetGenerator:
+    def __init__(self):
+        pass
+
+    def generate(self):
+        pass
+
