61d72a396d274ed9336b4758391980c136721109
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-05-13 12:44:30 +0300

    Changed config for transtimegan and run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-for i in $(seq 5 50); do python3 main.py --perplexity=$i; done
+for i in $(seq 30 10 50); do python3 main.py --perplexity=$i; done
+#for (( COUNTER=10; COUNTER<=50; COUNTER+=10 )); do
+#    python3 main.py --perplexity=$COUNTER
+#
+#done
