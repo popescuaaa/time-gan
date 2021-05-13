7246b067dd6a2bc3d3ec4eb5b8d43754c435ddac
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-05-13 04:03:12 +0300

    Experiment with Decoder at inference

--- Transformer.py
+++ Transformer.py
@@ -4,7 +4,7 @@ from torch import Tensor
 import torch.nn.functional as F
 import math
 from torch.nn import TransformerEncoder, TransformerEncoderLayer, TransformerDecoder, TransformerDecoderLayer
-from typing import Dict
+from typing import Dict, Tuple
 import wandb
 from data import Energy
 from torch.utils.data import DataLoader
@@ -35,11 +35,11 @@ def _generate_square_subsequent_mask(sz: int) -> Tensor:
 class Encoder(nn.Module):
     def __init__(self, cfg: Dict):
         super(Encoder, self).__init__()
-        self.feature_size = int(cfg['transformer']['feature_size'])
-        self.num_layers = int(cfg['transformer']['num_layers'])
-        self.dropout = float(cfg['transformer']['dropout'])
-        self.n_head = int(cfg['transformer']['n_head'])  # 10
-        self.dim_output = int(cfg['transformer']['dim_output'])
+        self.feature_size = int(cfg['encoder']['feature_size'])
+        self.num_layers = int(cfg['encoder']['num_layers'])
+        self.dropout = float(cfg['encoder']['dropout'])
+        self.n_head = int(cfg['encoder']['n_head'])  # 10
+        self.dim_output = int(cfg['encoder']['dim_output'])
 
         self.model_type = 'Transformer'
         self.src_mask = None
@@ -51,12 +51,12 @@ class Encoder(nn.Module):
                                                      dropout=self.dropout,
                                                      dim_feedforward=self.feature_size * 4)
         self.encoder = TransformerEncoder(encoder_layer=self.encoder_layer, num_layers=self.num_layers)
-        self.decoder = nn.Linear(in_features=self.feature_size, out_features=self.dim_output)
+        self.ll = nn.Linear(in_features=self.feature_size, out_features=self.dim_output)
 
         # Init weights
         init_range = 0.1
-        self.decoder.bias.data.zero_()
-        self.decoder.weight.data.uniform_(-init_range, init_range)
+        self.ll.bias.data.zero_()
+        self.ll.weight.data.uniform_(-init_range, init_range)
 
     def forward(self, src: Tensor) -> Tensor:
         if self.src_mask is None or self.src_mask.size(0) != len(src):
@@ -66,7 +66,42 @@ class Encoder(nn.Module):
 
         src = self.pos_encoder(src)
         output = self.encoder(src, self.src_mask)
-        output = self.decoder(output)
+        output = self.ll(output)
+        return output
+
+    @property
+    def device(self):
+        return next(self.parameters()).device
+
+
+class Decoder(nn.Module):
+    def __init__(self, cfg: Dict):
+        super(Decoder, self).__init__()
+        self.feature_size = int(cfg['decoder']['feature_size'])
+        self.num_layers = int(cfg['decoder']['num_layers'])
+        self.dropout = float(cfg['decoder']['dropout'])
+        self.n_head = int(cfg['decoder']['n_head'])
+        self.dim_output = int(cfg['decoder']['dim_output'])
+        self.model_type = 'Transformer'
+
+        # Architecture
+        self.pos_encoder = PositionalEncoding(dim_model=self.feature_size)
+        self.decoder_layer = TransformerDecoderLayer(d_model=self.feature_size,
+                                                     nhead=self.n_head,
+                                                     dropout=self.dropout,
+                                                     dim_feedforward=self.feature_size * 4)
+        self.decoder = TransformerDecoder(decoder_layer=self.decoder_layer, num_layers=self.num_layers)
+        self.ll = nn.Linear(in_features=self.feature_size, out_features=self.dim_output)
+
+        # Init weights
+        init_range = 0.1
+        self.ll.bias.data.zero_()
+        self.ll.weight.data.uniform_(-init_range, init_range)
+
+    def forward(self, tgt: Tensor, memory: Tensor) -> Tensor:
+        tgt = self.pos_encoder(tgt)
+        output = self.decoder(tgt, memory)
+        output = self.ll(output)
         return output
 
     @property
@@ -85,7 +120,7 @@ def run_transformer_test():
     # out = transformer_model(src, tgt)
 
     cfg = {
-        'transformer': {
+        'encoder': {
             'feature_size': 28,
             'num_layers': 1,
             'dropout': 0.1,
@@ -101,11 +136,25 @@ def run_transformer_test():
 
 # Training step
 def _embedding_forward_step(emb: Encoder,
-                            rec: Encoder,
-                            src: Tensor):
+                            rec: Decoder,
+                            src: Tensor,
+                            batch_size: int,
+                            seq_len: int) -> Tuple[Tensor, Tensor]:
     assert src.device == emb.device, 'Src and model are not on the same device'
     h = emb(src)
-    _src = rec(h)
+
+    # initial_sup_input = torch.zeros(size=(batch_size, 1, emb.dim_output))  # 0s
+    tgt = torch.zeros_like(h)
+    # print(initial_sup_input[batch_size - 1, 1, :].shape)
+    # tgt[batch_size - 1, 0, :] = initial_sup_input
+
+    for index in range(seq_len - 1):
+        _src = rec(tgt, h)
+        # print(_src.shape)
+        # print(tgt[0, index + 1, :].shape)
+        # print(_src[0, index, :].shape)
+        tgt[0, index + 1, :] = _src[0, index, :emb.dim_output]
+
     emb_loss = F.mse_loss(_src, src)
     return emb_loss, _src
 
@@ -115,19 +164,14 @@ if __name__ == '__main__':
     ds = Energy.Energy(seq_len=24)
     dl = DataLoader(ds, num_workers=2, batch_size=128, shuffle=True)
     cfg = {
-        'transformer': {
+        'encoder': {
             'feature_size': 28,
             'num_layers': 10,
             'dropout': 0.1,
             'n_head': 7,
             'dim_output': 14
-        }
-    }
-    emb = Encoder(cfg=cfg)
-    emb_opt = torch.optim.Adam(emb.parameters(), lr=1e-4)
-
-    cfg = {
-        'transformer': {
+        },
+        'decoder': {
             'feature_size': 14,
             'num_layers': 10,
             'dropout': 0.1,
@@ -135,7 +179,9 @@ if __name__ == '__main__':
             'dim_output': 28
         }
     }
-    rec = Encoder(cfg=cfg)
+    emb = Encoder(cfg=cfg)
+    emb_opt = torch.optim.Adam(emb.parameters(), lr=1e-4)
+    rec = Decoder(cfg=cfg)
     rec_opt = torch.optim.Adam(rec.parameters(), lr=1e-4)
     device = torch.device('cuda:0')
 
@@ -152,7 +198,7 @@ if __name__ == '__main__':
             emb.zero_grad()
             rec.zero_grad()
 
-            loss, _src = _embedding_forward_step(emb=emb, rec=rec, src=rd)
+            loss, _src = _embedding_forward_step(emb=emb, rec=rec, src=rd, batch_size=128, seq_len=24)
 
             loss.backward()
             rec_opt.step()
